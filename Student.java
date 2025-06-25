9066dd712aaf9e9eeff31944eaf53cd02c0007c3
sanjiv261 <sanjivgupta6263@gmail.com>
2025-06-25 22:14:35 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,17 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
-public class StudentRecordSystem {
-    private ArrayList<Student> students = new ArrayList<>();
-    private Scanner scanner = new Scanner(System.in);
-    private int nextId = 1;
-
-    public static void main(String[] args) {
-        StudentRecordSystem system = new StudentRecordSystem();
-        system.run();
-    }
-
 class Student {
     private int id;
     private String name;
